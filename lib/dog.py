b30262b9eb7b626532d280b4af6d9804e12cdffc
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-22 19:58:09 +0300

    completed assignments

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,23 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    def set_name(self, name):
+        if (isinstance(name, str)) and (1 <= len(name) <= 25):
+            print(f"Breed must be in list of approved breeds.")
+            self._name = name.title()
+        elif name == False:
+            print('Name must be string between 1 and 25 characters.')
+
+        else:
+            print('Name must be string between 1 and 25 characters.')
+    name = property(get_name, set_name)  
     pass
+
+snoop=Dog("snoop")
+print(snoop.name)
